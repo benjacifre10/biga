f3ff17d1d77abe9a869d3cb741d1301f45ab49c7
Benjamin Cifre <bcifre@teco.com.ar>
2022-06-30 17:10:28 -0300

    Updates

---
+++ precache-manifest.7bcaebff71f9928572e15d8bf20cb59c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8915e554a1d8bc2fb2f7",
+    "url": "./static/css/main.aeb5b62a.chunk.css"
+  },
+  {
+    "revision": "8915e554a1d8bc2fb2f7",
+    "url": "./static/js/main.263b7901.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "25b0bdc3e0159bf7216e",
+    "url": "./static/css/2.e58db57a.chunk.css"
+  },
+  {
+    "revision": "25b0bdc3e0159bf7216e",
+    "url": "./static/js/2.83e677b1.chunk.js"
+  },
+  {
+    "revision": "fc8af37db4e1738f23837ddce1ab529e",
+    "url": "./index.html"
+  }
+];
